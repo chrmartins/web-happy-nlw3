2896cefa709ece3fd23ff2188bb3f8fc9dd07151
chrmartins <cmts2010@gmail.com>
2020-10-15 19:33:37 -0300

    Segundo dia finalizado

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import '../src/styles/global.css'
+import Routes from './routes'
+
+
+function App() {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
